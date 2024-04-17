58469c3f1d9afbf14949db081ef45822b03bcfba
ANUP <anuppaulsookh@gmail.com>
2024-04-16 18:00:38 -0700

    no data found language update

--- lib/utils/no_data_found_widget.dart
+++ lib/utils/no_data_found_widget.dart
@@ -1,13 +1,14 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class NoDataFoundWidget extends StatelessWidget {
-  final String title;
+  // final String title;
   final String assetImage;
 
   const NoDataFoundWidget(
       {super.key,
-      this.title = 'No Data Found',
+      // this.title = 'No_data_found'.tr(),
       this.assetImage = 'assets/dashboard/no_data_found.json'});
 
   @override
@@ -19,7 +20,7 @@ class NoDataFoundWidget extends StatelessWidget {
       children: [
         Lottie.asset(assetImage, repeat: false, height: 200),
         Text(
-          title,
+          'No_data_found'.tr(),
           style: TextStyle(
               color: Colors.black.withOpacity(0.4),
               fontSize: 18,
